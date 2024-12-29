4dacb3bdced6bfae787b5c9e683fb5f503d38421
mitsukiraikiri <mitsukiraikiri@gmail.com>
2024-12-29 15:23:10 +0800

    up

---
+++ laravel/FileHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EntercodeHelper\Laravel;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class FileHelper
+{
+    protected $disk;
+    public function __construct($disk = null)
+    {
+        $this->disk = $disk ?? config('filesystems.default');
+    }
+    public function upload(string $folder, $file, string $filename = null)
+    {
+        $extension = $file->getClientOriginalExtension();
+        $filename = $filename ?? Str::uuid() . '-' . time() . '.' . $extension;
+        return $file->storeAs($folder, $filename, ['disk' => $this->disk]);
+    }
+
+    public function delete(string $path)
+    {
+        $path = str_replace(Storage::disk($this->disk)->url('/'), '', $path);
+        if (Storage::exists($path)) {
+            return Storage::disk($this->disk)->delete($path);
+        }
+    }
+}
---
+++ php/PaymentGateway.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace EntercodeHelper\PHP;
+
+class PaymentGateway {
+    
+}
